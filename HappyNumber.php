f6433603bd947da1bc77606d5ffff4f328a67733
Adriano Silva Belísio <adriano.belisio@yahoo.com.br>
2023-02-10 18:26:29 -0300

    numeros Felizes

---
+++ HappyNumber.php
@@ -0,0 +1,36 @@
+<?php
+class HappyNumber {
+    private $seen;
+
+    function isHappyNumber($num) {
+        $this->seen = array();
+
+        while ($num != 1 && !in_array($num, $this->seen)) {
+            array_push($this->seen, $num);
+            $num = $this->sumOfSquaredDigits($num);
+        }
+
+        return $num == 1;
+    }
+
+    function sumOfSquaredDigits($num) {
+        $sum = 0;
+
+        while ($num > 0) {
+            $digit = $num % 10;
+            $sum += $digit * $digit;
+            $num = floor($num / 10);
+        }
+
+        return $sum;
+    }
+}
+
+$happyNumber = new HappyNumber();
+$num = 7;
+
+if ($happyNumber->isHappyNumber($num)) {
+    echo "$num é um número feliz.";
+} else {
+    echo "$num não é um número feliz.";
+}
